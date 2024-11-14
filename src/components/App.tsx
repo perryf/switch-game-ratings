c210fce558b9afac780b7a4ba182abe45f904c02
Perry Fustero <pjfust@gmail.com>
2024-11-14 12:19:05 -0500

    comment out unused vars

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,22 @@
 import { useEffect, useState } from 'react'
-import { generateClient } from 'aws-amplify/data'
+// import { generateClient } from 'aws-amplify/data'
 // import { StorageImage } from '@aws-amplify/ui-react-storage'
 import type { Schema } from '../../amplify/data/resource'
 // import { ownedGamesReviewable } from '../switch-games-owned'
 // import switchGameListFull from '../switch-games-list-full.json'
 // @ts-ignore
 import { switchGamesOwnedMasterList } from '../../switch-games-owned-details'
-import { SwitchGameBasic } from '../interfaces'
-import CreateGameForm from './createGameForm/CreateGameForm'
+// import { SwitchGameBasic } from '../interfaces'
+// import CreateGameForm from './createGameForm/CreateGameForm'
 import SwitchGameList from './switchGameList/SwitchGameList'
 
-interface AllGames {
-  title: string
-  description: string
-  genres: string[]
-}
+// interface AllGames {
+//   title: string
+//   description: string
+//   genres: string[]
+// }
 
-const client = generateClient<Schema>()
+// const client = generateClient<Schema>()
 
 // TODO -> Figure out why manually entered games did not get entered into the DB
 function App() {
@@ -42,32 +42,33 @@ function App() {
     )
   }, [])
 
-  async function createGame(newGame: SwitchGameBasic) {
-    // ? not sure if this is needed -- probably a better way
-    // turns comma separated strings into arrays, changes numbers that are technically strings into numbers
-    // const data: {
-    //   name: string
-    //   rating: number
-    //   [key: string]: string | boolean | number | string[]
-    // } = {
-    //   ...newGame,
-    //   mood: newGame.mood ? newGame.mood.split(',').map(m => m.trim()) : [],
-    //   tags: newGame.tags ? newGame.tags.split(',').map(m => m.trim()) : [],
-    //   multiplayerType: newGame.multiplayerType
-    //     ? newGame.multiplayerType.split(',').map(m => m.trim())
-    //     : [],
-    //   price: +newGame.price,
-    //   multiplayerNumberOfPlayers: +newGame.multiplayerNumberOfPlayers,
-    //   rating: +newGame.rating
-    // }
-    // // remove empty strings
-    // Object.keys(data).forEach((key: string) => {
-    //   if (data[key] === '') delete data[key]
-    // })
-    // const res = await client.models.Game.create(switchGamesOwnedMasterList[0])
-  }
+  // async function createGame(newGame: SwitchGameBasic) {
+  // ? not sure if this is needed -- probably a better way
+  // turns comma separated strings into arrays, changes numbers that are technically strings into numbers
+  // const data: {
+  //   name: string
+  //   rating: number
+  //   [key: string]: string | boolean | number | string[]
+  // } = {
+  //   ...newGame,
+  //   mood: newGame.mood ? newGame.mood.split(',').map(m => m.trim()) : [],
+  //   tags: newGame.tags ? newGame.tags.split(',').map(m => m.trim()) : [],
+  //   multiplayerType: newGame.multiplayerType
+  //     ? newGame.multiplayerType.split(',').map(m => m.trim())
+  //     : [],
+  //   price: +newGame.price,
+  //   multiplayerNumberOfPlayers: +newGame.multiplayerNumberOfPlayers,
+  //   rating: +newGame.rating
+  // }
+  // // remove empty strings
+  // Object.keys(data).forEach((key: string) => {
+  //   if (data[key] === '') delete data[key]
+  // })
+  // const res = await client.models.Game.create(switchGamesOwnedMasterList[0])
+  // }
 
   async function deleteGame(id: string) {
+    console.log(id)
     if (window.confirm('Are you sure you want to delete this game?')) {
       // await client.models.Game.delete({ id })
     }
