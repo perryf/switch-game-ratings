66be862ed8343ddabb4160d6b30218f7caffff38
Perry Fustero <pjfust@gmail.com>
2024-12-02 00:27:22 -0500

    undo changes made to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
-  if (command === 'build') {
-    return {
-      plugins: [react()],
-      base: '/switch-game-ratings/'
-    }
-  }
-
-  return {
-    plugins: [react()]
-  }
+export default defineConfig({
+  plugins: [react()]
 })
