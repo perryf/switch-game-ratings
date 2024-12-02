9e3abc82bc0fd4ad5f41769b00bb9f132ab989c9
Perry Fustero <pjfust@gmail.com>
2024-12-02 00:20:04 -0500

    new try to get deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  if (command === 'build') {
+    return {
+      plugins: [react()],
+      base: '/switch-game-ratings/'
+    }
+  }
+
+  return {
+    plugins: [react()]
+  }
 })
